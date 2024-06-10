9940647bb01d3c3a3b00a4f6dfe1c7de4c13ed41
D-13 <D-13@D-13>
2024-06-10 20:25:03 +0700

    Upload Project Tebak Angka

---
+++ TebakAngka/src/main/java/com/asia/tebakangka/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asia.tebakangka;
+
+/**
+ *
+ * @author D-13
+ */
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(10);
+        game.play();
+    }
+}
